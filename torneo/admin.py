c90252ed3acbde6e46e616eeadd7a26cf0815280
Enrico Polesel <enrico.polesel@sns.it>
2016-02-15 17:57:48 +0100

    Calcolo automatico del paramentro done della partita con errore in caso di ambiguità

--- torneo/admin.py
+++ torneo/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from torneo.models import Squadra,Partita
 
+from django.contrib import messages
+
 # Register your models here.
 
 class PartitaAdmin(admin.ModelAdmin):
-    #fields = ['data','squadra1','squadra2','punteggio1','punteggio2']
+    fields = ['data','squadra1','squadra2','punteggio1','punteggio2']
     list_display = ['data','squadra1','squadra2','punteggio1','punteggio2', 'done']
     list_filter = ['data','done']
 
@@ -13,14 +15,19 @@ class PartitaAdmin(admin.ModelAdmin):
         instance = form.save(commit=False)
         if (instance.punteggio1 > 0) and (instance.punteggio2 > 0):
             instance.done = True
+        elif (instance.punteggio1 == 0) and (instance.punteggio2 == 0):
+            instance.done = False
+        else:
+            messages.error(request, 'Non capisco se sia stata disputata la partita o no, quindi non salvo!')
+            return instance
         instance.save()
+        form.save_m2m()
         # instance.squadra1.ripunteggia()
         # instance.squadra2.ripunteggia()        
         # instance.squadra1.save()
         # instance.squadra2.save()
         for squadra in Squadra.objects.all():
             squadra.ripunteggia()
-        form.save_m2m()
         return instance
         
 
